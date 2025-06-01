e7e068ee4f8404e2ea06af1eafc7a93bf0835ed5
OmegaOne <omegaone2025@outlook.com>
2025-06-02 01:16:43 +0900

    fix: husky config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -10,11 +10,11 @@ export default function Home() {
           alt="Next.js logo"
           width={180}
           height={38}
-               priority
+          priority
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            Get started by editing{' '}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
               src/app/page.tsx
             </code>
